1e89cb8a816b981fdc26f54733f74448b6626f50
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-27 18:12:19 +0530

    add page for the course creation.

---
+++ pages/courses/create.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Create a Course</h1>
+        <form @submit.prevent="submitForm">
+            <label for="name">Name:</label>
+            <input id="name" v-model="form.name"/>
+
+            <label for="description">Description:</label>
+            <textarea id="description" v-model="form.description"></textarea>
+
+            <button type="submit">Create Course</button>
+        </form>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const form = ref({name: 'Science 101', description: 'Introduction to basic Science.'});
+const router = useRouter();
+
+const submitForm = async () => {
+    await $fetch('/api/courses', { method: "POST", body: form.value });
+    await router.push('/courses');
+};
+</script>
