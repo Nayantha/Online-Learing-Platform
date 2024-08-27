230a3b26d743a074ff5f3cebd148e355c17f0d68
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-27 17:55:30 +0530

    create a course edit api endpoint.

---
+++ server/api/courses/[id].put.ts
@@ -0,0 +1,24 @@
+import { prisma } from '~/server/db';
+import { Course } from '~/utils/types';
+
+export default defineEventHandler(async (event: any) => {
+    const updatedCourse: Course = await readBody(event);
+
+    const courseId: number = Number(getRouterParam(event, 'id'));
+
+    console.log(updatedCourse,courseId)
+
+    try {
+        await prisma.course.update({
+            where: {
+                id: courseId,
+            },
+            data: {
+                ...updatedCourse,
+            },
+        });
+    } catch (error) {
+        console.error('Error updating course:', error);
+        throw error;
+    }
+});
