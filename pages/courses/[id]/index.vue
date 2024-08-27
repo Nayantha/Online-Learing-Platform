1acc9eb6272a0c50114d59205d961d932177467f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-27 18:40:20 +0530

    add function to delete a course.

--- pages/courses/[id]/index.vue
+++ pages/courses/[id]/index.vue
@@ -3,18 +3,31 @@
         <h1>{{ course.name }}</h1>
         <p>{{ course.description }}</p>
         <NuxtLink :to="`/courses/${course.id}/edit`">Edit Course</NuxtLink>
+        <button @click="deleteCourse">Delete</button>
     </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import type { Course } from "~/utils/types";
 
+const router = useRouter();
 const route = useRoute();
 const course  = ref<Course | null>(null);
 
 onMounted(async () => {
     course.value = await $fetch(`/api/courses/${ route.params.id }`);
 });
+
+const deleteCourse = async () => {
+    if (confirm('Are you sure you want to delete this course?')) {
+        try {
+            await $fetch(`/api/courses/${ route.params.id }`, { method: 'DELETE' });
+            // await router.push('/courses');
+        } catch (error) {
+            console.error('Failed to delete the course:', error);
+        }
+    }
+};
 </script>
