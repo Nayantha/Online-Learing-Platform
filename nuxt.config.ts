7dace8c9bc93f95cabc889f44a7266451c48563a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-27 12:57:02 +0530

    add prisma storage setting.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
+    // @ts-ignore
     compatibilityDate: '2024-08-27',
     devtools: { enabled: true },
     modules: [
         '@pinia/nuxt',
         "@prisma/nuxt"
     ],
+    piniaPersistedstate: {
+        storage: 'localStorage'
+    }
 })
