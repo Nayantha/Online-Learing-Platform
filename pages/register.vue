14a0da566bdaef745949c43eb2e986cb7b8f0ddf
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-27 15:46:59 +0530

    add register page.

---
+++ pages/register.vue
@@ -0,0 +1,65 @@
+<script lang="ts" setup>
+
+import { ref } from 'vue';
+import axios from 'axios';
+
+export interface RegisterBody {
+    email: string;
+    password: string;
+    name: string;
+}
+
+const form = ref<RegisterBody>({
+    email: 'email@mail.com',
+    password: '1234567890',
+    name: 'name1',
+});
+
+const submitForm = async () => {
+    try {
+        const response = await axios.post('/api/auth/register', form.value);
+        console.log('Form submitted:', form.value);
+        console.log(response)
+    } catch (error) {
+        console.error('Submission error:', error);
+    }
+};
+</script>
+<template>
+    <form @submit.prevent="submitForm">
+        <div>
+            <label for="name">Name:</label>
+            <input
+                    id="name"
+                    v-model="form.name"
+                    placeholder="Enter your name"
+                    required
+                    type="text"
+            />
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input
+                    id="email"
+                    v-model="form.email"
+                    placeholder="Enter your email"
+                    required
+                    type="email"
+            />
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input
+                    id="password"
+                    v-model="form.password"
+                    placeholder="Enter your password"
+                    required
+                    type="password"
+            />
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+</template>
+<style scoped>
+
+</style>
