0dd3d1261aea6bb302506081a6633820ee497531
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-27 14:30:58 +0530

    get prisma from predefine file.

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,11 +1,13 @@
+// @ts-ignore
 import bcrypt from 'bcrypt';
+// @ts-ignore
 import jwt from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '~/server/db'
 
-// Define types for request body
 interface AuthBody {
     email: string;
     password: string;
+    name: string;
 }
 
 // Define the type of response
@@ -18,26 +20,27 @@ interface AuthResponse {
     };
 }
 
-// Create an instance of PrismaClient
-const prismaClient = new PrismaClient();
-
 export default defineEventHandler(async (event: AuthResponse): Promise<AuthResponse> => {
     const body: AuthBody = await readBody(event);
-    const { email, password } = body;
+    const { email, password, name } = body;
 
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    const count = await prisma.student.count();
+
     // Create a new user
-    const user = await prismaClient.user.create({
+    const user = await prisma.student.create({
         data: {
             email,
             password: hashedPassword,
+            name: name,
+            std_id: `ST\\00${count+1}\\`
         },
     });
 
     // Generate a JWT token
-    const token = jwt.sign({ userId: user.id }, 'your-secret-key', { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user.id }, '4b7e35d3d60d3d8461efea6e33c6fa7b', { expiresIn: '1h' });
 
     // Return the response
     return { token, user: { id: user.id, email: user.email, password: hashedPassword } };
