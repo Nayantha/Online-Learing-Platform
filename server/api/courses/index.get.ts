d43833ee9ddcf37058df65c8aa11875f4ec2ebc8
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-27 17:08:12 +0530

    add types to the return.

--- server/api/courses/index.get.ts
+++ server/api/courses/index.get.ts
@@ -1,5 +1,6 @@
 import { prisma } from '~/server/db';
+import { Course } from '~/utils/types';
 
 export default defineEventHandler(async () => {
-    return prisma.course.findMany();
+    return prisma.course.findMany() as Course[];
 });
