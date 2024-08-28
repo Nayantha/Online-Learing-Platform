90dddd498d9b5afc0aa82e9832585b85514cd93d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-28 15:33:30 +0530

    add course enrollment endpoint.

---
+++ server/api/enrollment/index.post.ts
@@ -0,0 +1,25 @@
+// @ts-ignore
+import {defineEventHandler, H3Event, readBody} from 'h3';
+import {CourseCreationResponseBody, CourseEnrollmentBody} from "~/utils/types";
+// @ts-ignore
+import jwt from "jsonwebtoken";
+import {prisma} from "~/server/db";
+
+export default defineEventHandler(async (event: H3Event<Request>): Promise<CourseCreationResponseBody> => {
+    const token = getHeader(event, 'Authorization')?.replace('Bearer ', '');
+    const body: CourseEnrollmentBody = await readBody(event);
+    try {
+        const secret = process.env.JWT_SECRET as string;
+        jwt.verify(token, secret);
+
+        await prisma.course.create({
+            data: {
+                ...body
+            }
+        });
+        return {"message": `Student ${body.studentId} is now enrolled in Course ${body.courseId}.`} as CourseCreationResponseBody;
+
+    } catch (e) {
+        return {"message": "Something went wrong"} as CourseCreationResponseBody;
+    }
+})
