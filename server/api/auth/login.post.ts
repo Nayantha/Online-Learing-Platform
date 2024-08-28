9814cbbc99ca099a96b28b55895f24cec83eca4d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-28 09:28:55 +0530

    add types to variables.

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -2,32 +2,30 @@
 import bcrypt from 'bcrypt';
 // @ts-ignore
 import jwt from 'jsonwebtoken';
-import { prisma } from '~/server/db'
-import { createSession } from '~/server/sessionManager';
-import { AuthResponse, LoginAuthBody } from "~/utils/types";
+import {prisma} from '~/server/db'
+import {createSession} from '~/server/sessionManager';
+import {AuthResponse, LoginAuthBody, Session, Student} from "~/utils/types";
 
 export default defineEventHandler(async (event: AuthResponse): Promise<AuthResponse> => {
     const body: LoginAuthBody = await readBody(event);
-    const { email, password } = body;
+    const {email, password} = body;
 
-    const student = await prisma.student.findUniqueOrThrow({
+    const student: Student = await prisma.student.findUniqueOrThrow({
         where: {
             email: email
         },
-    });
+    }) as Student;
 
-    const isPasswordValid = await bcrypt.compare(password, student.password);
+    const isPasswordValid: boolean = await bcrypt.compare(password, student.password);
 
     if (isPasswordValid) {
 
-        const session = await createSession({userId: student.id, userType: "student"});
+        const session: Session = await createSession({userId: student.id, userType: "student"});
         return {
             session: session,
-            token: session.token, user: { id: student.id, email: student.email }, message : "ok"
+            token: session.token, user: {id: student.id, email: student.email}, message: "ok"
         };
     } else {
-        return { token: null, user: null, "message": "wrong password", session: null}
+        return {token: null, user: null, "message": "wrong password", session: null}
     }
-
-
 });
