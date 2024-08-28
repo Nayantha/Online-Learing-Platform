543af234d6372ea870e5112a5a437bc85181231d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-28 15:08:58 +0530

    add JWT verification.

--- server/api/courses/[id].get.ts
+++ server/api/courses/[id].get.ts
@@ -1,11 +1,27 @@
-import { prisma } from '~/server/db';
-import { Course } from '~/utils/types';
+import {prisma} from '~/server/db';
+import {Course} from '~/utils/types';
+// @ts-ignore
+import jwt from "jsonwebtoken";
 
 export default defineEventHandler(async (event: any) => {
     const courseId: number = Number(getRouterParam(event, 'id'));
-    return prisma.course.findUniqueOrThrow({
-        where: {
-            id: courseId
+    const token = getHeader(event, 'Authorization')?.replace('Bearer ', '');
+
+    try {
+        const secret = process.env.JWT_SECRET as string;
+        jwt.verify(token, secret);
+
+        return prisma.course.findUniqueOrThrow({
+            where: {
+                id: courseId
+            }
+        }) as Course;
+    } catch (error: any) {
+        if (error.name === 'TokenExpiredError') {
+            console.error('Token has expired:', error.expiredAt);
+        } else {
+            console.error('Error:', error.message);
         }
-    }) as Course;
+        return [] as Course[];
+    }
 });
