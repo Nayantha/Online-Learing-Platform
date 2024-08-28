7e445315b0d29c785af84695902695fb883bc1a1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-28 09:35:24 +0530

    refactor variables.

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -3,28 +3,26 @@ import bcrypt from 'bcrypt';
 // @ts-ignore
 import jwt from 'jsonwebtoken';
 import {prisma} from '~/server/db'
-import {AuthResponse, RegisterAuthBody} from '~/utils/types';
+import {AuthResponse, RegisterAuthBody, Student} from '~/utils/types';
 
 export default defineEventHandler(async (event: AuthResponse): Promise<AuthResponse> => {
     const body: RegisterAuthBody = await readBody(event);
     const {email, password, name} = body;
 
-    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const count = await prisma.student.count();
 
-    // Create a new user
-    const user = await prisma.student.create({
+    const student: Student = await prisma.student.create({
         data: {
             email,
             password: hashedPassword,
             name: name,
-            std_id: `ST\\00${count+1}\\`
+            std_id: `ST\\00${count + 1}\\`
         },
-    });
+    }) as Student;
 
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+    const token = jwt.sign({userId: student.id}, process.env.JWT_SECRET as string, {expiresIn: '1h'});
 
-    return {session: null, token, user: {id: user.id, email: user.email}, message: "Successfully registered."};
+    return {session: null, token, user: {id: student.id, email: student.email}, message: "Successfully registered."};
 });
