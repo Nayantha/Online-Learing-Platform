f03822de27a79f9fae59fc204c6a167b6f887930
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-28 09:39:55 +0530

    add try and use session manager.

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -3,26 +3,34 @@ import bcrypt from 'bcrypt';
 // @ts-ignore
 import jwt from 'jsonwebtoken';
 import {prisma} from '~/server/db'
-import {AuthResponse, RegisterAuthBody, Student} from '~/utils/types';
+import {AuthResponse, RegisterAuthBody, Session, Student} from '~/utils/types';
+import {createSession} from "~/server/sessionManager";
 
 export default defineEventHandler(async (event: AuthResponse): Promise<AuthResponse> => {
     const body: RegisterAuthBody = await readBody(event);
     const {email, password, name} = body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+        const count = await prisma.student.count();
 
-    const count = await prisma.student.count();
+        const student: Student = await prisma.student.create({
+            data: {
+                email,
+                password: hashedPassword,
+                name: name,
+                std_id: `ST\\00${count + 1}\\`
+            },
+        }) as Student;
 
-    const student: Student = await prisma.student.create({
-        data: {
-            email,
-            password: hashedPassword,
-            name: name,
-            std_id: `ST\\00${count + 1}\\`
-        },
-    }) as Student;
-
-    const token = jwt.sign({userId: student.id}, process.env.JWT_SECRET as string, {expiresIn: '1h'});
-
-    return {session: null, token, user: {id: student.id, email: student.email}, message: "Successfully registered."};
+        const session: Session = await createSession({userId: student.id, userType: "student"});
+        return {
+            session: session,
+            token: session.token,
+            user: {id: student.id, email: student.email},
+            message: "ok"
+        };
+    } catch (e) {
+        return {session: null, token: null, user: null, message: "Something went wrong"};
+    }
 });
