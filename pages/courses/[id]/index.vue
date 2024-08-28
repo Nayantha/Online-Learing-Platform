61cf1055b6938a3098cde5a9647dc17e2d876494
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-28 15:59:35 +0530

    display delete button with if condition.

--- pages/courses/[id]/index.vue
+++ pages/courses/[id]/index.vue
@@ -3,18 +3,19 @@
         <h1>{{ course.name }}</h1>
         <p>{{ course.description }}</p>
         <NuxtLink :to="`/courses/${course.id}/edit`">Edit Course</NuxtLink>
-        <button @click="deleteCourse">Delete</button>
+        <button v-if="isAdmin" @click="deleteCourse">Delete</button>
     </div>
 </template>
 
 <script lang="ts" setup>
 import {onMounted, ref} from 'vue';
 import {useRoute, useRouter} from 'vue-router';
-import type {Course} from "~/utils/types";
+import type {Course, Session} from "~/utils/types";
 
 const router = useRouter();
 const route = useRoute();
 const course = ref<Course | null>(null);
+const isAdmin = ref(false);
 
 onMounted(async () => {
     course.value = await $fetch(`/api/courses/${route.params.id}`, {
@@ -23,12 +24,22 @@ onMounted(async () => {
             Authorization: `Bearer ${localStorage.getItem('token')}`
         }
     });
+
+    // Ensure this runs only in the client
+    if (process.client) {
+        const session: Session | null = localStorage.getItem('session') as Session | null;
+        if (session) {
+            if (session.userType === 'admin') {
+                isAdmin.value = true;
+            }
+        }
+    }
 });
 
 const deleteCourse = async () => {
     if (confirm('Are you sure you want to delete this course?')) {
         try {
-            await $fetch(`/api/courses/${ route.params.id }`, {
+            await $fetch(`/api/courses/${route.params.id}`, {
                 method: 'DELETE',
                 headers: {
                     Authorization: `Bearer ${localStorage.getItem('token')}`
