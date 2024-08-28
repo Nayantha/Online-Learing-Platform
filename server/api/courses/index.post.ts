435b0ef94e34a3bd5e0faad472c723a19b6949b8
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-28 15:14:34 +0530

    add JWT admin only verification path.

--- server/api/courses/index.post.ts
+++ server/api/courses/index.post.ts
@@ -1,16 +1,28 @@
-import { CourseCreationBody, CourseCreationResponseBody } from "~/utils/types";
-import { prisma } from "~/server/db";
+import {CourseCreationBody, CourseCreationResponseBody} from "~/utils/types";
+import {prisma} from "~/server/db";
+// @ts-ignore
+import jwt from "jsonwebtoken";
 
 
 export default defineEventHandler(async (event: CourseCreationBody): Promise<CourseCreationResponseBody> => {
     const body: CourseCreationBody = await readBody(event);
+    const token = getHeader(event, 'Authorization')?.replace('Bearer ', '');
+
     try {
-        await prisma.course.create({
-            data: {
-                ...body
-            }
-        });
-        return { "message": `Course ${ body.name } created.` } as CourseCreationResponseBody;
+        const secret = process.env.JWT_SECRET as string;
+        const decodedJWT = jwt.verify(token, secret);
+
+        if (decodedJWT.userType === "admin") {
+            await prisma.course.create({
+                data: {
+                    ...body
+                }
+            });
+            return {"message": `Course ${body.name} created.`} as CourseCreationResponseBody;
+        } else {
+            throw createError({statusCode: 403, statusMessage: 'Forbidden: Admins only'});
+        }
+
     } catch (e) {
         return { "message": "Something went wrong" } as CourseCreationResponseBody;
     }
