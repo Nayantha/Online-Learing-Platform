10998a4f4a678526f9c56ff221f0c6561b7392eb
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-28 11:01:00 +0530

    add admin login UI page.

---
+++ pages/admin/login.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+
+import {ref} from 'vue';
+import type {LoginAuthBody} from "~/utils/types";
+
+const form = ref<LoginAuthBody>({
+    email: 'email@mail.com',
+    password: '1234567890',
+});
+
+const authStore = useAuthStore();
+
+const submitForm = async () => {
+    try {
+        await authStore.loginAdmin({...form.value});
+    } catch (error) {
+        console.error('Submission error:', error);
+    }
+};
+</script>
+<template>
+    <form @submit.prevent="submitForm">
+        <div>
+            <label for="email">Email:</label>
+            <input
+                    id="email"
+                    v-model="form.email"
+                    placeholder="Enter your email"
+                    required
+                    type="email"
+            />
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input
+                    id="password"
+                    v-model="form.password"
+                    placeholder="Enter your password"
+                    required
+                    type="password"
+            />
+        </div>
+        <button type="submit">Submit</button>
+    </form>
+</template>
+<style scoped>
+
+</style>
