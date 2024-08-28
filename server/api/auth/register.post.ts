6a57b7a569c3ce61d39657e8632ce18a3a4ef5ec
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-08-28 09:33:52 +0530

    implement members of interface.

--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -2,12 +2,12 @@
 import bcrypt from 'bcrypt';
 // @ts-ignore
 import jwt from 'jsonwebtoken';
-import { prisma } from '~/server/db'
-import { AuthResponse, RegisterAuthBody } from '~/utils/types';
+import {prisma} from '~/server/db'
+import {AuthResponse, RegisterAuthBody} from '~/utils/types';
 
 export default defineEventHandler(async (event: AuthResponse): Promise<AuthResponse> => {
     const body: RegisterAuthBody = await readBody(event);
-    const { email, password, name } = body;
+    const {email, password, name} = body;
 
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -26,5 +26,5 @@ export default defineEventHandler(async (event: AuthResponse): Promise<AuthRespo
 
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
 
-    return { token, user: { id: user.id, email: user.email }, message: "Successfully registered." };
+    return {session: null, token, user: {id: user.id, email: user.email}, message: "Successfully registered."};
 });
